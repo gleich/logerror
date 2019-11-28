d870975df5036192ecdcc23e70a68d3db2ecca9e
Matthew Gleich <matthewgleich@gmail.com>
2019-11-28 13:33:40 -0500

    Print before log

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/fatih/color"
+	log "github.com/sirupsen/logrus"
+)
 
-// LogErr ... Logs an error if there is an error with logrus
-func LogErr(err error) {
+// Log ... Logs an error if there is an error with logrus
+func Log(err error, msg string) {
 	if err != nil {
+		if msg != "" {
+			color.Red(msg)
+		}
 		log.Fatal(err)
 	}
 }
