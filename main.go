65bc131f20ac701ef839dc3eb39c0471ab4e5d83
Matthew Gleich <matthewgleich@gmail.com>
2019-11-28 13:39:49 -0500

    Log ERROR and err can be any type

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 // Log ... Logs an error if there is an error with logrus
-func Log(err error, msg string) {
+func Log(err interface{}, msg string) {
 	if err != nil {
 		if msg != "" {
 			color.Red(msg)
 		}
-		log.Fatal(err)
+		log.Error(err)
 	}
 }
